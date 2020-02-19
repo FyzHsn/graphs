1d244df09d7c4ed6f7b0d879507549756ec39e83
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2020-02-19 09:25:43 -0500

    Add method to find node geodesics

--- utils.py
+++ utils.py
@@ -63,3 +63,46 @@ def preprocess(text, stop_filter=True, pos_filter=True):
         cleaned_sentence_list.append(sentence)
 
     return cleaned_sentence_list
+
+
+def node_geodesic(graph, node_1, node_2):
+    """Geodesic between two nodes
+
+    This method finds the shortest path between two nodes in a graph. This
+    function can be applied to any connected or disconnected undirected graphs.
+
+    :param graph: node relation of the undirected graph
+    :type graph: dict of the form {int: list}
+    :param node_1: first node of interest
+    :type node_1: int
+    :param node_2: second node of interest
+    :type node_2: int
+    :return: None (if the points are disconnected) or the shortest distance
+    between the two.
+    :rtype: None or int
+    """
+
+    paths = [node_1]
+    finished_traverses = []
+
+    while len(paths):
+        new_gen_paths = []
+        for path in paths:
+            for adj_node in graph[path[-1]]:
+                if adj_node == node_2:
+                    finished_traverses.append(path + adj_node)
+                elif adj_node not in path:
+                    new_gen_paths.append(path + adj_node)
+        paths = new_gen_paths
+
+    return min([len(path) for path in finished_traverses])
+
+
+if __name__ == "__main__":
+    G = {'1': ['2', '3'],
+         '2': ['1', '3'],
+         '3': ['1', '2'],
+         '4': ['5'],
+         '5': ['4']}
+
+    print(node_geodesic(G, '1', '3'))
